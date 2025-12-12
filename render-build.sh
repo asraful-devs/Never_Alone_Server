38ba19bb2786fb8d2f3fc60f8fb9a86a649e1c68
MdAsraful56 <work.mdasraful56@gmail.com>
2025-12-12 19:29:31 +0600

    update deploy

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+npm install --save-dev @types/express @types/cors @types/cookie-parser @types/bcrypt @types/jsonwebtoken @types/multer @types/nodemailer
 npm install
 npm run build
 npx prisma generate
