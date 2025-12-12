7f4ddedbac4858ccb34d0573a0df43bdc44902a9
MdAsraful56 <work.mdasraful56@gmail.com>
2025-12-12 21:15:04 +0600

    ...

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,12 @@ set -o errexit
 
 npm install --save-dev @types/express @types/cors @types/cookie-parser @types/bcrypt @types/jsonwebtoken @types/multer @types/nodemailer
 npm install
+
+# render-build.sh
+npm install
+npx prisma generate --schema=./prisma/schema
+echo "TypeScript build skipped for deployment"
+
 npm run build
 npx prisma generate
 npx prisma migrate deploy
