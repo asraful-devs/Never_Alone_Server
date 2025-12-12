1858bd1680a725bb8d59075ed7a0dca8d642a8c4
MdAsraful56 <work.mdasraful56@gmail.com>
2025-12-12 23:21:02 +0600

    some update

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,34 +1,32 @@
-import express, { NextFunction, Request, Response } from "express";
-import { fileUploader } from "../../helpers/fileUploader";
-import { AdminController } from "./admin.controller";
-import { AdminValidation } from "./admin.validation";
-import validateRequest from "../../middlewares/validateRequest";
-import auth from "../../middlewares/auth";
-import { Role } from "@prisma/client";
+import express, { NextFunction, Request, Response } from 'express';
+import { fileUploader } from '../../helpers/fileUploader';
+import validateRequest from '../../middlewares/validateRequest';
+import { AdminController } from './admin.controller';
+import { AdminValidation } from './admin.validation';
 
 const router = express.Router();
 
 router.post(
-  "/create-admin",
-  validateRequest(AdminValidation.createAdminValidationSchema),
-  AdminController.CreateAdmin,
+    '/create-admin',
+    validateRequest(AdminValidation.createAdminValidationSchema),
+    AdminController.CreateAdmin
 );
 
-router.get("/get-all-admin", auth(Role.ADMIN), AdminController.GetAllAdmin);
+router.get('/get-all-admin', AdminController.GetAllAdmin);
 
-router.get("/get-single-admin/:id", AdminController.GetSingleAdmin);
+router.get('/get-single-admin/:id', AdminController.GetSingleAdmin);
 
 router.patch(
-  "/update-admin/:id",
-  fileUploader.upload.single("file"),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = AdminValidation.updateAdminValidationSchema.parse(
-      JSON.parse(req.body.data),
-    );
-    return AdminController.UpdateAdmin(req, res, next);
-  },
+    '/update-admin/:id',
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = AdminValidation.updateAdminValidationSchema.parse(
+            JSON.parse(req.body.data)
+        );
+        return AdminController.UpdateAdmin(req, res, next);
+    }
 );
 
-router.delete("/soft-delete-admin/:id", AdminController.DeleteAdmin);
+router.delete('/soft-delete-admin/:id', AdminController.DeleteAdmin);
 
 export const adminRoutes = router;
--- src/app/modules/host/host.route.ts
+++ src/app/modules/host/host.route.ts
@@ -1,34 +1,32 @@
-import express, { NextFunction, Request, Response } from "express";
-import validateRequest from "../../middlewares/validateRequest";
-import { HostController } from "./host.controller";
-import { HostValidation } from "./host.validation";
-import auth from "../../middlewares/auth";
-import { Role } from "@prisma/client";
-import { fileUploader } from "../../helpers/fileUploader";
+import express, { NextFunction, Request, Response } from 'express';
+import { fileUploader } from '../../helpers/fileUploader';
+import validateRequest from '../../middlewares/validateRequest';
+import { HostController } from './host.controller';
+import { HostValidation } from './host.validation';
 
 const router = express.Router();
 
 router.post(
-  "/create-host",
-  validateRequest(HostValidation.createHostZodSchema),
-  HostController.CreateHost,
+    '/create-host',
+    validateRequest(HostValidation.createHostZodSchema),
+    HostController.CreateHost
 );
 
-router.get("/get-all-host", auth(Role.ADMIN), HostController.GetAllHost);
+router.get('/get-all-host', HostController.GetAllHost);
 
-router.get("/get-single-host/:id", HostController.GetSingleHost);
+router.get('/get-single-host/:id', HostController.GetSingleHost);
 
 router.patch(
-  "/update-host/:id",
-  fileUploader.upload.single("file"),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = HostValidation.updateHostZodSchema.parse(
-      JSON.parse(req.body.data),
-    );
-    return HostController.UpdateHost(req, res, next);
-  },
+    '/update-host/:id',
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = HostValidation.updateHostZodSchema.parse(
+            JSON.parse(req.body.data)
+        );
+        return HostController.UpdateHost(req, res, next);
+    }
 );
 
-router.delete("/soft-delete-host/:id", HostController.DeleteHost);
+router.delete('/soft-delete-host/:id', HostController.DeleteHost);
 
 export const hostRoutes = router;
